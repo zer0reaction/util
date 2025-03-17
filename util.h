fd1f5fbac282cf951904fd9aff9806751537e684
zer0reaction <boltgopnik@gmail.com>
2025-03-17 10:43:56 +0300

    Removed arena.end, typedef bool, debug message changes

--- util.h
+++ util.h
@@ -4,7 +4,6 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <stdbool.h>
 
 #ifdef UTIL_DEBUG
     #include <stdio.h>
@@ -37,6 +36,10 @@
 
 #define list_create(arena, T, size) internal_list_create(arena, size, sizeof(T))
 
+typedef unsigned char bool;
+#define true 1
+#define false 0
+
 typedef struct Arena_Region Arena_Region;
 struct Arena_Region {
     Arena_Region *next;
@@ -48,8 +51,8 @@ struct Arena_Region {
 
 typedef struct Arena Arena;
 struct Arena {
-    Arena_Region *start, *end;
-    Arena_Region *start_reallocatable, *end_reallocatable;
+    Arena_Region *start;
+    Arena_Region *start_reallocatable;
 };
 
 typedef struct List_Header List_Header;
@@ -93,7 +96,7 @@ Arena_Region *internal_arena_region_create_reallocatable(size_t bytes) {
     size_t region_capacity;
 
     r = malloc(sizeof(Arena_Region));
-    region_capacity = bytes * ARENA_REGION_REALLOC_FACTOR;
+    region_capacity = UTIL_MAX(ARENA_REGION_DEFAULT_CAPACITY, bytes * ARENA_REGION_REALLOC_FACTOR);
 
     r->next = NULL;
     r->capacity = region_capacity;
@@ -110,7 +113,6 @@ Arena_Region *internal_get_ptr_region(Arena *a, void *ptr) {
     Arena_Region *r;
 
     r = a->start_reallocatable;
-
     while (r != NULL) {
         if (ptr == r->data) return r;
         UTIL_DEBUG_INFO(("Going through reallocatable regions..."));
@@ -132,7 +134,7 @@ Arena_Region *internal_get_ptr_region(Arena *a, void *ptr) {
 void internal_arena_region_push(Arena *a, Arena_Region *r) {
     if (a->start == NULL) {
         UTIL_DEBUG_INFO(("Pushing regular region to an empty list"));
-        a->start = a->end = r;
+        a->start = r;
     } else {
         UTIL_DEBUG_INFO(("Pushing regular region"));
         r->next = a->start;
@@ -143,7 +145,7 @@ void internal_arena_region_push(Arena *a, Arena_Region *r) {
 void internal_arena_region_push_reallocatable(Arena *a, Arena_Region *r) {
     if (a->start_reallocatable == NULL) {
         UTIL_DEBUG_INFO(("Pushing reallocatable region to an empty list"));
-        a->start_reallocatable = a->end_reallocatable = r;
+        a->start_reallocatable = r;
     } else {
         UTIL_DEBUG_INFO(("Pushing reallocatable region"));
         r->next = a->start_reallocatable;
@@ -175,10 +177,10 @@ void *arena_alloc(Arena *a, size_t bytes) {
         return malloc(bytes);
     }
 
-    if (a->start == NULL || a->end->used + bytes > a->end->capacity) {
+    if (a->start == NULL || a->start->used + bytes > a->start->capacity) {
         Arena_Region *r;
 
-        UTIL_DEBUG_INFO(("Creating a new region and allocating %ld bytes on it", bytes));
+        UTIL_DEBUG_INFO(("There are no regions yet or not enough space in the start region, creating a new one and allocating %ld bytes on it", bytes));
 
         r = internal_arena_region_create(bytes);
         internal_arena_region_push(a, r);
@@ -187,8 +189,8 @@ void *arena_alloc(Arena *a, size_t bytes) {
 
     UTIL_DEBUG_INFO(("Allocating %ld bytes on an existing region", bytes));
 
-    ptr = (char *)a->end->data + a->end->used;
-    a->end->used += bytes;
+    ptr = (char *)a->start->data + a->start->used;
+    a->start->used += bytes;
     return ptr;
 }
 
@@ -256,8 +258,8 @@ void arena_free(Arena *a) {
         r = next_region;
     }
 
-    a->start = a->end = NULL;
-    a->start_reallocatable = a->end_reallocatable = NULL;
+    a->start = NULL;
+    a->start_reallocatable = NULL;
 }
 
 size_t list_get_size(void *list) {
