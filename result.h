a6bae57e1f625dc9be16dff25a5434f76f2409ec
zer0reaction <boltgopnik@gmail.com>
2025-03-25 03:08:11 +0300

    Added comment to result.h

--- result.h
+++ result.h
@@ -8,6 +8,17 @@ enum Result_Code {
 };
 typedef enum Result_Code Result_Code;
 
+/*
+  Result structs are defined like this:
+
+  typedef struct {
+    Result_Code code;
+    int value;
+  } Result_Int;
+
+  Then you can use this struct for the macros.
+*/
+
 #define UTIL_RESULT_OK(r, v) do { r.code = UTIL_RESULT_OK; r.value = v; } while (0)
 #define UTIL_RESULT_ERR(r) do { r.code = UTIL_RESULT_ERR; } while (0)
 #define UTIL_RESULT_UNWRAP(r) (assert(r.code == UTIL_RESULT_OK), r.value)
