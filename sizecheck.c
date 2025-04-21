bb178868b1fe04a937b107039fe094ddc9f7d657
zer0reaction <boltgopnik@gmail.com>
2025-04-22 00:30:25 +0300

    [sizecheck] indent 2 -> 4

--- sizecheck.c
+++ sizecheck.c
@@ -4,23 +4,23 @@
 #include <assert.h>
 
 int main(void) {
-  assert(sizeof(i8) == 1);
-  assert(sizeof(i16) == 2);
-  assert(sizeof(i32) == 4);
-  assert(sizeof(i64) == 8);
+    assert(sizeof(i8) == 1);
+    assert(sizeof(i16) == 2);
+    assert(sizeof(i32) == 4);
+    assert(sizeof(i64) == 8);
 
-  assert(sizeof(u8) == 1);
-  assert(sizeof(u16) == 2);
-  assert(sizeof(u32) == 4);
-  assert(sizeof(u64) == 8);
+    assert(sizeof(u8) == 1);
+    assert(sizeof(u16) == 2);
+    assert(sizeof(u32) == 4);
+    assert(sizeof(u64) == 8);
 
-  assert(sizeof(f32) == 4);
-  assert(sizeof(f64) == 8);
+    assert(sizeof(f32) == 4);
+    assert(sizeof(f64) == 8);
 
-  assert(sizeof(c8) == 1);
-  assert(sizeof(c16) == 2);
+    assert(sizeof(c8) == 1);
+    assert(sizeof(c16) == 2);
 
-  INFO("sizecheck", ("All types match their sizes"));
+    INFO("sizecheck", ("All types match their sizes"));
 
-  return 0;
+    return 0;
 }
